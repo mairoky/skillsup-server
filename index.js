77d23cd6e839c99a05e79bbaea8ec811280f23d8
Aminul Islam Rocky <mairoky@outlook.com>
2022-10-26 15:17:49 +0600

    get course data from server

--- index.js
+++ index.js
@@ -6,8 +6,21 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const courses = require('./data/courses.json');
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Server Running!');
+})
+
+app.get('/course', (req, res) => {
+    res.send(courses);
+})
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find(c => c.id === id);
+    // console.log(selectedCourse);
+    res.send(selectedCourse);
 })
 
 app.listen(port, () => {
